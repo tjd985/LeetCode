c9074a653b6a98996699534a6a621512f96efc2f
OH SeongHo <tjd985@gmail.com>
2024-04-01 15:47:06 +0900

    Time: 74 ms (8.63%), Space: 53.3 MB (5.54%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+const mergeTwoLists = function(list1, list2) {
+  const valueList = [];
+  
+  if (!(list1 || list2)) {
+    return list1
+  }
+  
+  addValue(valueList, list1);
+  addValue(valueList, list2);
+  
+  valueList.sort((a, b) => a - b);
+  
+  console.log(new ListNode(null));
+  
+  const headNode = new ListNode(valueList[0] ? valueList[0] : null);
+  
+  for (let i = 1; i < valueList.length; i++) {
+    const newNode = new ListNode(valueList[i]);
+    let currentNode = headNode;
+    
+    while (true) {
+      if (!currentNode.next) {
+        currentNode.next = newNode;
+        
+        break;
+      }
+      
+      currentNode = currentNode.next;
+    }
+  }
+  
+  return headNode;
+};
+
+function addValue(valueList, targetList) {
+  while (targetList) {
+    valueList.push(targetList.val);
+    
+    targetList = targetList.next;
+  }
+}
