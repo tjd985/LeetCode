5c86439d1db0cddab7d0c0b081f657e79fb1aafe
OH SeongHo <tjd985@gmail.com>
2024-04-02 15:09:09 +0900

    Time: 82 ms (17.66%), Space: 54 MB (12.89%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,36 @@
+const floodFill = function(image, sr, sc, color) {
+  const moveX = [-1, 0, 1, 0];
+  const moveY = [0, -1, 0, 1];
+  const isVisit = new Array(image.length)
+       .fill(null)
+       .map((value) => new Array(image[0].length)
+       .fill(false));
+  const queue = [[sr, sc]];
+  const startColor = image[sr][sc];
+
+  while(queue.length) {
+    const [currentX, currentY] = queue.shift();
+
+    isVisit[currentX][currentY] = true;
+    image[currentX][currentY] = color;
+
+    for (let i = 0; i < 4; i++) {
+      const nextX = currentX + moveX[i];
+      const nextY = currentY + moveY[i];
+      const isValid = nextX < image.length &&
+            nextX < image[0].length &&
+            nextX >=0 &&
+            nextY >= 0 &&
+            image[nextX][nextY] === startColor &&
+            !isVisit[nextX][nextY];
+
+      if (!isValid) {
+        continue;
+      }
+
+      queue.push([nextX, nextY]);
+    }
+  }
+
+  return image;
+}
