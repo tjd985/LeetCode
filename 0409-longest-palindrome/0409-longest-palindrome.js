49a516ea63854c6b64701007b5c4e0126576664c
OH SeongHo <tjd985@gmail.com>
2024-04-15 22:35:18 -0700

    Time: 74 ms (5.17%), Space: 50.6 MB (65.74%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -1,10 +1,16 @@
 var longestPalindrome = function(s) {
   let ans = 0;
-  let keys = {};
+  const keys = {};
   
-  for (let char of s) {
+  for (const char of s) {
     keys[char] = (keys[char] || 0) + 1;
-    if (keys[char] % 2 == 0) ans += 2;
+    
+    if (keys[char] % 2 == 0) {
+      ans += 2;
+    }
   }
+  
+  console.log(ans);
+  
   return s.length > ans ? ans + 1 : ans;
 };
