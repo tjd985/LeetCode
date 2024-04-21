e8b7b1c885ba7828d405076d8a2bb639c3faad19
OH SeongHo <tjd985@gmail.com>
2024-04-20 21:08:33 -0700

    Time: 82 ms (100.00%), Space: 51.8 MB (100.00%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+const numberOfSpecialChars = function(word) {
+  const wordObject = {};
+  let result = new Set();
+  
+  function isUpperCase(character) {
+    return character === character.toUpperCase();
+  }
+  
+  for (const character of word) {
+    const isUpper = isUpperCase(character);
+    const upperCase = character.toUpperCase();
+    const lowerCase = character.toLowerCase();
+    
+    wordObject[character] = true;
+    
+     if (isUpper) {
+       if (wordObject[lowerCase]) {
+         result.add(lowerCase);
+       }
+       
+       continue;
+     }
+    
+    if (wordObject[upperCase]) {
+       result.add(lowerCase);
+     }
+  }
+  
+  return result.size;
+};
