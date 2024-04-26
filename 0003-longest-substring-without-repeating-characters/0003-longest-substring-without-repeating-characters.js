8b959a36cf981b29a427187acdaa938384f1275b
OH SeongHo <tjd985@gmail.com>
2024-04-26 05:27:40 -0700

    Time: 353 ms (6.11%), Space: 58.9 MB (8.14%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function(s) {
+  let currentString = "";
+  let maxLength = 0;
+  const currentCharacter = {};
+  
+  for (let i = 0; i < s.length; i++) {
+    const targetCharacter = s[i];
+    
+    if (currentCharacter[targetCharacter]) {
+      delete currentCharacter[targetCharacter];
+      
+      for (let i = currentString.length - 1; i >= 0; i--) {
+        const currentCharacter = currentString[i];
+        
+        if (currentCharacter === targetCharacter) {
+          currentString = currentString.slice(i + 1);
+          
+          break;
+        }
+      }
+    }
+    
+    currentString += targetCharacter;
+    currentCharacter[targetCharacter] = (currentCharacter[targetCharacter] || 0) + 1;
+    
+    if (currentString.length >= maxLength) {
+        maxLength = currentString.length;
+      }
+    
+    console.log(currentString, maxLength);
+  }
+  
+  return maxLength ? maxLength : currentString.length;
+};
