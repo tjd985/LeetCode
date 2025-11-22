52dc8b106680a7803b6f733f3d128bfd81a723c4
SEONGHO OH <tjd985@gmail.com>
2025-11-22 22:04:20 +0900

    Time: 1083 ms (5.11%), Space: 72.6 MB (36.94%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+  const zeroCount = nums.filter((value) => !value).length;
+
+  if (zeroCount >= 2) {
+    return new Array(nums.length).fill(0);
+  }
+
+  const beforeMultiple = [];
+  const afterMultiple = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0) {
+      beforeMultiple.push(1);
+      afterMultiple.push(1);
+
+      continue;
+    }
+
+    beforeMultiple.push(beforeMultiple[i - 1] * nums[i - 1]);
+    afterMultiple.unshift(afterMultiple[0] * nums[nums.length - i]);
+  }
+
+  const answer = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = beforeMultiple[i] * afterMultiple[i];
+  }
+
+  return answer;
+};
