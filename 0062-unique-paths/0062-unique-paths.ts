2df4c00c0687ac004be87c330df6d1df2c1039f8
SEONGHO OH <tjd985@gmail.com>
2025-12-21 18:53:27 +0900

    Time: 1 ms (60.04%), Space: 55 MB (73.98%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -0,0 +1,11 @@
+function uniquePaths(m: number, n: number): number {
+  const dp = new Array(m).fill(false).map((value) => new Array(n).fill(1));
+
+  for (let i = m - 2; i >= 0; i--) {
+    for (let j = n - 2; j >= 0; j--) {
+      dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+    }
+  }
+
+  return dp[0][0];
+};
