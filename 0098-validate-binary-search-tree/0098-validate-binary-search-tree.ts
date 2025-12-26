1cea688e32a271389cc0ec467651985fc95c3c57
SEONGHO OH <tjd985@gmail.com>
2025-12-26 23:31:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.ts
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+  class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.left = left === undefined ? null : left;
+      this.right = right === undefined ? null : right;
+    }
+  }
+
+  function recursion(currentNode: TreeNode, min: number, max: number) {
+    const currentValue = currentNode.val;
+
+    let leftResult = true;
+    let rightResult = true;
+
+    // left
+    if (currentNode.left !== null) {
+      const leftValue = currentNode.left.val;
+      const newMax = currentValue;
+
+      if (leftValue > min && leftValue < newMax) {
+        leftResult = recursion(currentNode.left, min, newMax);
+      } else {
+        return false;
+      }
+    }
+
+    // right
+    if (currentNode.right !== null) {
+      const rightValue = currentNode.right.val;
+      const newMin = currentValue;
+
+      if (rightValue >= newMin && rightValue < max) {
+        rightResult = recursion(currentNode.right, newMin, max);
+      } else {
+        return false;
+      }
+    }
+
+    return leftResult && rightResult;
+  }
+
+  return recursion(root, -Infinity, Infinity);
+};
