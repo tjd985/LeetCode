99feb1aedf4291c84d86a51bc1f5b4491bb3d6fb
Seongho Oh <tjd985@gmail.com>
2025-12-29 23:47:44 +0900

    Time: 1830 ms (5.6%), Space: 67 MB (40%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.ts
@@ -0,0 +1,34 @@
+function leastInterval(tasks: string[], n: number): number {
+  const taskMap = new Map;
+  const idleMap = {};
+
+  tasks.forEach((task) => {
+    taskMap.set(task, (taskMap.get(task) ?? 0) + 1);
+  });
+
+  let time = 0;
+  let completed = 0;
+
+  while (true) {
+    if (completed === tasks.length) {
+      return time;
+    }
+
+    const list = Array.from(taskMap.entries()).sort((a, b) => a[1] - b[1]);
+
+    for (let i = list.length - 1; i >= 0; i--) {
+      const task = list[i][0];
+      const canProcess = (idleMap[task] === undefined || idleMap[task] < time) && taskMap.get(task) > 0;
+
+      if (canProcess) {
+        idleMap[task] = time + n;
+        taskMap.set(task, taskMap.get(task) - 1);
+        completed++;
+
+        break;
+      }
+    }
+
+    time++;
+  }
+};
